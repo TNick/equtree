9bbaf307d78cdb8a57d60d6997fe6435bcafc2c9
Nicu Tofan <nicu.tofan@gmail.com>
2013-05-13 14:48:19 +0300

    Initial rails new project

---
+++ EquTree/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EquTree::Application.config.secret_token = '0f558f9bc2155863f7b5f4e7d62d5b00f4f83095f6d5e991b04a380a7becb2a330143f78cf5d7eed6850e645a75c6c2ad23f7eb4eeaa74ed5d286ccff197e853'
